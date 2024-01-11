a51f674be0550dc6dd6ccc2825e55a92d56a185f
justine92415 <justine92415@gmail.com>
2024-01-11 23:36:15 +0800

    新增 表單元件-輸入欄位（雛形）

--- src/app/pages/components-demo/components-demo.component.ts
+++ src/app/pages/components-demo/components-demo.component.ts
@@ -3,11 +3,12 @@ import { ButtonHeroComponent } from '../../components/button-hero/button-hero.co
 import { ButtonComponent } from '../../components/button/button.component';
 import { SelectComponent } from '../../components/select/select.component';
 import { CheckboxComponent } from '../../components/checkbox/checkbox.component';
+import { InputComponent } from '../../components/input/input.component';
 
 @Component({
   selector: 'app-components-demo',
   standalone: true,
-  imports: [ButtonHeroComponent,ButtonComponent,SelectComponent,CheckboxComponent],
+  imports: [ButtonHeroComponent,ButtonComponent,SelectComponent,CheckboxComponent,InputComponent],
   templateUrl: './components-demo.component.html',
   styleUrl: './components-demo.component.scss'
 })
