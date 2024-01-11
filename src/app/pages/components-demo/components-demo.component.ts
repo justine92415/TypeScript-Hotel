3de3e690049bbe0d25805a031232e870647d3aaf
justine92415 <justine92415@gmail.com>
2024-01-11 22:31:24 +0800

    新增表單元件-勾選方雛形）

--- src/app/pages/components-demo/components-demo.component.ts
+++ src/app/pages/components-demo/components-demo.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonHeroComponent } from '../../components/button-hero/button-hero.component';
 import { ButtonComponent } from '../../components/button/button.component';
 import { SelectComponent } from '../../components/select/select.component';
+import { CheckboxComponent } from '../../components/checkbox/checkbox.component';
 
 @Component({
   selector: 'app-components-demo',
   standalone: true,
-  imports: [ButtonHeroComponent,ButtonComponent,SelectComponent],
+  imports: [ButtonHeroComponent,ButtonComponent,SelectComponent,CheckboxComponent],
   templateUrl: './components-demo.component.html',
   styleUrl: './components-demo.component.scss'
 })
