d417044de53581b8817a4de402429122101f7128
justine92415 <justine92415@gmail.com>
2024-01-28 16:32:16 +0800

    新增 註冊頁 切版

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CheckboxComponent } from '../../components/checkbox/checkbox.component';
+import { InputComponent } from '../../components/input/input.component';
+import { StepperComponent } from '../../components/stepper/stepper.component';
+import { SelectComponent } from '../../components/select/select.component';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [InputComponent,CheckboxComponent,ButtonComponent,StepperComponent,SelectComponent],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export default class RegisterPageComponent {
-
+  step = 2;
 }
