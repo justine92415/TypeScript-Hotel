f6f4c8f65242f7d0c0ff25302425b3f195935473
justine92415 <justine92415@gmail.com>
2024-01-29 21:12:37 +0800

    新增 divider 元件

--- src/app/pages/layout/confirm-page/confirm-page.component.ts
+++ src/app/pages/layout/confirm-page/confirm-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
 import { InputComponent } from '../../../components/input/input.component';
 import { SelectComponent } from '../../../components/select/select.component';
+import { DividerComponent } from '../../../divider/divider.component';
 
 @Component({
   selector: 'app-confirm-page',
   standalone: true,
-  imports: [ButtonComponent,InputComponent,SelectComponent],
+  imports: [ButtonComponent,InputComponent,SelectComponent,DividerComponent],
   templateUrl: './confirm-page.component.html',
   styleUrl: './confirm-page.component.scss'
 })
