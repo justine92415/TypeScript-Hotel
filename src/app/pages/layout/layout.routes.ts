88acbe880d778ddbd517482e0cf471d5919ca6f7
justine92415 <justine92415@gmail.com>
2024-01-29 22:16:55 +0800

    新增 預約成功頁 切版

--- src/app/pages/layout/layout.routes.ts
+++ src/app/pages/layout/layout.routes.ts
@@ -22,6 +22,10 @@ export const routes: Routes = [
     path:'confirm',
     loadComponent: () => import('./confirm-page/confirm-page.component'),
   },
+  {
+    path:'success',
+    loadComponent: () => import('./success-page/success-page.component'),
+  }
 ];
 
 export default routes;
---
+++ src/app/pages/layout/success-page/success-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DividerComponent } from '../../../divider/divider.component';
+import { ButtonComponent } from '../../../components/button/button.component';
+
+@Component({
+  selector: 'app-success-page',
+  standalone: true,
+  imports: [DividerComponent,ButtonComponent],
+  templateUrl: './success-page.component.html',
+  styleUrl: './success-page.component.scss'
+})
+export default class SuccessPageComponent {
+
+}
